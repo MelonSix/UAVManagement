1df5816600ca4be4a1aeb452034bc37b586b54bc
BRIGHTER AGYEMANG <brighteragyemang@gmail.com>
2015-08-14 15:39:26 +0800

    Bootstrap impl - attempt to connect to server using bootstrap info...

--- ManagmentAdapter/src/main/java/org/mars/m2m/managmentadapter/service/OperationService.java
+++ ManagmentAdapter/src/main/java/org/mars/m2m/managmentadapter/service/OperationService.java
@@ -17,7 +17,6 @@ import org.mars.m2m.dmcore.onem2m.xsdBundle.ObjectFactory;
 import org.mars.m2m.managmentadapter.client.ServiceConsumer;
 import org.slf4j.LoggerFactory;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
